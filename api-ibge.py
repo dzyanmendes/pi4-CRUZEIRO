fcb316ba5e5e0ec02a82a8211ceb9d006e110e0c
Dzyan Mendes <34127377+dzyanmendes@users.noreply.github.com>
2023-04-17 12:23:24 -0300

    Update api-ibge.py

--- api-ibge.py
+++ api-ibge.py
@@ -13,14 +13,14 @@ data_response = response.data.decode("utf-8")
 data_json = json.loads(data_response)
 data_populacao = data_json["projecao"]
 data_grafico = data_json["projecao"]["periodoMedio"]
-totalNascimento = data_grafico["nascimento"]
-incrementoPop = data_grafico["incrementoPopulacional"]
-totalObito = data_grafico["obito"]
 dataUpdate = data_json["horario"]
 df= json_normalize(data_json["projecao"])
-df2= json_normalize(data_json["projecao"]["periodoMedio"])
-df3 = data_grafico
+df2 = data_grafico
 st.header('SIAD - IBGE - UNIVESP')
-st.bar_chart(df)
-st.bar_chart(df2)
-st.bar_chart(df3)
+st.markdown('Este sistema tem por objetivo, facilitar a visualização e centralização de consultas de informações do IBGE')
+if st.sidebar.button("População/Nascimentos/Óbitos"):
+ st.header("Projeção Populacional")
+ st.bar_chart(df)
+ st.bar_chart(df2)
+ st.markdown('Ultima Consulta:')
+ st.text(dataUpdate)
